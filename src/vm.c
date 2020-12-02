1e3182388da58c98dbf60c52e4a725c11fc42fcc
mixr26 <mihailo.stojanovic26@gmail.com>
2020-12-02 20:33:07 +0100

    Chapter 9: Local Variables

--- src/vm.c
+++ src/vm.c
@@ -121,6 +121,18 @@ static Interpret_result run() {
         case OP_POP:
             pop();
             break;
+        case OP_GET_LOCAL:
+        {
+            uint8_t slot = READ_BYTE();
+            push(vm.stack[slot]);
+            break;
+        }
+        case OP_SET_LOCAL:
+        {
+            uint8_t slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
         case OP_GET_GLOBAL:
         {
             Obj_string* name = READ_STRING();
